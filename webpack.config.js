b54eba29489d62385fdab6a71e213ff44ee88d1b
vivizuma <vitomanz@hotmail.co.uk>
2023-03-31 09:32:19 +0100

    Basic structure, .gitignore added

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 9000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.jpg$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
